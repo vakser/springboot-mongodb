b75aa3966b0e23eef28fc0192b4f9c6797c8dcfe
vakser <65089446+vakser@users.noreply.github.com>
2023-09-19 09:45:23 +0300

    REST end point created for adding new todo to the database.

--- src/main/java/com/example/springbootmongodb/controller/TodoController.java
+++ src/main/java/com/example/springbootmongodb/controller/TodoController.java
@@ -5,8 +5,11 @@ import com.example.springbootmongodb.repository.TodoRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Date;
 import java.util.List;
 
 @RestController
@@ -24,6 +27,17 @@ public class TodoController {
             return new ResponseEntity<>(todos, HttpStatus.OK);
         } else {
             return new ResponseEntity<>("No todos available", HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PostMapping("/todos")
+    public ResponseEntity<?> createTodo(@RequestBody TodoDTO todo) {
+        try {
+            todo.setCreatedAt(new Date(System.currentTimeMillis()));
+            todoRepo.save(todo);
+            return new ResponseEntity<>(todo, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 }
